7940c447e94523a27798d29b9036b7c41edbe5ec
harrypantsguy <harrypants2014@gmail.com>
2023-10-31 13:52:44 -0700

    imported dependencies including react bootstrap (may not end up being used) set the html page name and description imported flashgov logo and rendering it as a test, as well as a simple button with a count variable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import flashGovLogo from './flashGovLogo.png';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
+  const [clickCount, setClickCount] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img 
+          style={{width: '600px'}}
+          src={flashGovLogo} 
+          alt="logo"
+        />
+        <Button onClick={() => setClickCount(clickCount + 1)}>
+          Clicked {clickCount} times
+        </Button>
       </header>
     </div>
   );
 }
-
 export default App;
