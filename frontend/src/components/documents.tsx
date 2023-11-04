64f8857a58d766e1e0a6932496814a4635f30471
harrypantsguy <harrypants2014@gmail.com>
2023-11-03 17:11:35 -0700

    - Displayed documents now correspond to ones in the database, displaying title and a formatted date - Merged the frontend-alterations branch with the loadSpeeches branch - Fixed a bug with button hover colors - Made the search button smaller

--- frontend/src/components/documents.tsx
+++ frontend/src/components/documents.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../App.css';
 import DocumentCard from './documentCard';
 import SearchBar from './searchBar';
 import FilterPanel from './filterPanel';
+// Import speech obj interfaces
+import { Speech } from '../models/speechInterface';
 
 function Documents() {
 
@@ -10,6 +12,25 @@ function Documents() {
     const [specDate,toggleSpecDate] = useState(false);
     const [keyWords, setKeywords]:any = useState([]);
     const [currentWord,setWord] = useState("");
+    /* State of loaded speeches */
+    const [loadedSpeeches, setLoadedSpeeches] = useState<Speech[]>([]);
+
+    useEffect(()=>{
+      async function loadSpeeches(){
+        try {
+          // Query Server for speeches
+          // Able to leave out the localhost/5000 portion because of proxy in package.json
+          const response = await fetch("/api/speeches", {method:"GET"});
+          const returnedSpeeches = await response.json();
+          setLoadedSpeeches(returnedSpeeches);
+          console.log("speeches:", loadedSpeeches)
+        } catch (error) {
+          console.error(error);
+          alert(error);
+        }
+      }
+      loadSpeeches();
+    }, []);
 
     const toggleFilterCallback = () => {
       setWord("");
@@ -27,7 +48,7 @@ function Documents() {
     const pushKeywordCallback = () => {
       if (currentWord) setKeywords([...keyWords, currentWord]);
     }
-
+    
     const documentData:any[] = [
       {
         title: "H. Con. Res. 1 (Engrossed in House) - Regarding consent to assemble outside the seat of government.",
@@ -58,7 +79,7 @@ function Documents() {
         {/* Displays all the document date */}
         <div className='document-container'>
           {!documentData.length && <h2>No Results</h2>}
-          {documentData.map((document,index) => (<DocumentCard key={index} title={document.title} date={document.date} summary={document.summary} speaker={document.speaker}/>))}
+          {loadedSpeeches.map((document,index) => (<DocumentCard key={index} title={document.title} date={new Date(document.date).toLocaleDateString("en-us", {year:"numeric", month: "long", day: "numeric"})} summary={document.summary ? document.summary! : ""} speaker={document.speaker}/>))}
         </div>
 
       </div>
