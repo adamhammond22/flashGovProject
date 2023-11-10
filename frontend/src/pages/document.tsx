f507e82ae0b5e2094bac45a89d15d4eaca639a3f
adamhammond22 <abhammond22@gmail.com>
2023-11-10 11:48:49 -0800

    Added helpers.tsx to the frontend
    - helpers.tsx added
    - parseAndFormatDate is now used on all frontend

--- frontend/src/pages/document.tsx
+++ frontend/src/pages/document.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom"
+import { parseAndFormatDate } from "../components/helpers";
 
 function Document() {
 
     // Getting document ID From url parameter
     const {id} = useParams()
     const [speechInfo,setSpeechInfo] = useState({title:"",speaker:"",
-    date:new Date(),text:"",section:"",
+    date: "" ,text:"",section:"",
     summary:""})
 
     const [collapsed, toggleCollapsed] = useState(true);
@@ -17,9 +18,9 @@ function Document() {
            try {
                 const res = await fetch(`/api/speeches/${id}`);
                 const resJson = await res.json()
-                const dateObject = new Date(resJson.date)
+                const dateString = parseAndFormatDate(resJson.date)
                 const decodeText = atob(resJson.text)
-                setSpeechInfo({...resJson, date:dateObject,text:decodeText});
+                setSpeechInfo({...resJson, date:dateString,text:decodeText});
            } catch (e) {
             alert(e);
            }
@@ -27,18 +28,13 @@ function Document() {
         getSpeechInfo();
     },[id])
 
-    const formattedDate = new Intl.DateTimeFormat('en-US', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-      }).format(speechInfo.date);
     return (
         <div className="App">
             <div className="individual-document wrapper">
                 <Link className="link" to="/"><button className="back">Back</button></Link>
                 <h1>{speechInfo.title}</h1>
                 <hr/>
-                <h3>Date of Speech: {formattedDate}</h3>
+                <h3>Date of Speech: {speechInfo.date}</h3>
                 <h3>Speaker: {speechInfo.speaker}</h3>
                 <h3>Chamber: {speechInfo.section}</h3>
                 <div className="summary-container">
