8289a52b02ff93f9a923f625eb7b2b8c35a42b8c
harrypantsguy <harrypants2014@gmail.com>
2023-11-14 13:31:03 -0800

    - Added URL to the speech creation function in speechesController.ts

--- backend/src/controllers/speechesController.ts
+++ backend/src/controllers/speechesController.ts
@@ -62,6 +62,7 @@ interface CreateSpeechBody {
     date: Date,
     speaker: string,
     section: string, 
+    url: string,
     summary?: string,
 }
 
@@ -74,6 +75,7 @@ export const createSpeech: RequestHandler<unknown, unknown, CreateSpeechBody, un
     const givenSpeaker = req.body.speaker;
     const givenSection = req.body.section;
     const givenSummary = req.body.summary;
+    const givenURL = req.body.url;
     try {
         // Ensure Required fields were provided
         if(!givenTitle) {
@@ -86,6 +88,8 @@ export const createSpeech: RequestHandler<unknown, unknown, CreateSpeechBody, un
             throw createHttpError(400, "Speech must have a speaker");
         } else if(!givenSection) {
             throw createHttpError(400, "Speech must have a legislative body / section");
+        } else if(!givenURL) {
+            throw createHttpError(400, "Speech must have a url");
         }
 
         // Check for invalid date format
@@ -100,6 +104,7 @@ export const createSpeech: RequestHandler<unknown, unknown, CreateSpeechBody, un
             date: givenDate,
             speaker: givenSpeaker,
             section: givenSection,
+            url: givenURL,
             summary: givenSummary,
         });
 
