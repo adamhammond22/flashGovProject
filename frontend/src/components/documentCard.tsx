7403f7e726fc482628d0d991c77fd22d42de2947
JRN03 <jonathan.nguyen2021@gmail.com>
2023-12-03 17:58:24 -0800

    Added speaker to card

--- frontend/src/components/documentCard.tsx
+++ frontend/src/components/documentCard.tsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 function DocumentCard (props: {id:String, title: string, date: string, speaker:string}) {
     return (
         <div className="document-card">
-            <p>{props.title}</p>
+            <div className="card-speaker-title">
+                <h1>{props.title}</h1>
+                <p>{props.speaker}</p>
+            </div>
             <div className="date-view-container">
                 <p>{props.date}</p>
                 <Link to={`/document/${props.id}`}><button>View</button></Link>
